fdfdaabf5e0b34dfe1a394012ff8a6410802c901
Xlee25 <enesc5741@gmail.com>
2021-01-26 16:54:47 +0300

    Add project files.

---
+++ Dictonary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictonary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> Ogrenciler = new Dictionary<int, string>();
+
+            Ogrenciler.Add(1537,"Enes Karatas");
+            Ogrenciler.Add(1445,"Mahmut Kaplan");
+
+            foreach (var Ogrenci in Ogrenciler)
+            {
+                Console.WriteLine(Ogrenci);
+            }
+        }
+    }
+}
